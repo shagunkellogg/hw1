7c60d425c4220f359fa8194b816284b22ad17e4d
Shagun Sharma <shagun.sharma@kellogg.northwestern.edu>
2025-01-29 23:51:58 +0000

    Added tables

--- kmdb.sql
+++ kmdb.sql
@@ -111,14 +111,44 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+DROP TABLE IF EXISTS studio;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS character;
+DROP TABLE IF EXISTS movies;
 
 -- Create new tables, according to your domain model
--- TODO!
+
+CREATE TABLE studio(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    studio_name TEXT
+);
+
+CREATE TABLE actors(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    actor_name TEXT
+);
+
+CREATE TABLE character(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_id INTEGER,
+    actor_id INTEGER,
+    name TEXT
+);
+
+CREATE TABLE movies(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    studio_id INTEGER,
+    character_id INTEGER,
+    actor_id INTEGER,
+    title TEXT,
+    year_released INTEGER,
+    MPAA_rating TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+
+
 
 -- Prints a header for the movies output
 .print "Movies"
