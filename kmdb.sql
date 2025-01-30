e6371dcfd6c13182e00b9430f9526d10f104b77d
Shagun Sharma <shagun.sharma@kellogg.northwestern.edu>
2025-01-30 00:22:44 +0000

    First complete draft

--- kmdb.sql
+++ kmdb.sql
@@ -148,7 +148,62 @@ CREATE TABLE movies(
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 
+INSERT INTO studio(
+    studio_name
+) 
+VALUES (
+    'Warner Bros.'
+);
 
+INSERT INTO movies(
+    studio_id, 
+    title, 
+    year_released, 
+    MPAA_rating
+) 
+VALUES
+    (1, 'Batman Begins', 2005, 'PG-13'),
+    (1, 'The Dark Knight', 2008, 'PG-13'),
+    (1, 'The Dark Knight Rises', 2012, 'PG-13'
+);
+
+INSERT INTO actors(
+    actor_name
+)
+ VALUES 
+    ('Christian Bale'),
+    ('Michael Caine'),
+    ('Liam Neeson'),
+    ('Katie Holmes'),
+    ('Gary Oldman'),
+    ('Heath Ledger'),
+    ('Aaron Eckhart'),
+    ('Maggie Gyllenhaal'),
+    ('Tom Hardy'),
+    ('Joseph Gordon-Levitt'),
+    ('Anne Hathaway'
+);
+
+INSERT INTO character(
+    movie_id, actor_id, name
+) 
+VALUES 
+    (1, 1, 'Bruce Wayne'),
+    (1, 2, 'Alfred'),
+    (1, 3, 'Ra''s Al Ghul'),
+    (1, 4, 'Rachel Dawes'),
+    (1, 5, 'Commissioner Gordon'),
+    (2, 1, 'Bruce Wayne'),
+    (2, 6, 'Joker'),
+    (2, 7, 'Harvey Dent'),
+    (2, 2, 'Alfred'),
+    (2, 8, 'Rachel Dawes'),
+    (3, 1, 'Bruce Wayne'),
+    (3, 5, 'Commissioner Gordon'),
+    (3, 9, 'Bane'),
+    (3, 10, 'John Blake'),
+    (3, 11, 'Selina Kyle'
+);
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -156,7 +211,11 @@ CREATE TABLE movies(
 .print ""
 
 -- The SQL statement for the movies output
--- TODO!
+
+SELECT movies.title, movies.year_released, movies.MPAA_rating, studio.studio_name 
+FROM movies 
+    INNER JOIN studio ON movies.studio_id = studio.id
+ORDER BY movies.id ASC;
 
 -- Prints a header for the cast output
 .print ""
@@ -166,4 +225,8 @@ CREATE TABLE movies(
 
 
 -- The SQL statement for the cast output
--- TODO!
+SELECT movies.title, actors.actor_name, character.name 
+FROM character
+    INNER JOIN movies ON character.movie_id = movies.id
+    INNER JOIN actors ON character.actor_id = actors.id
+ORDER BY movies.year_released, movies.title;
